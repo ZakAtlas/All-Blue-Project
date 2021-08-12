cfa1285837b540460600db883eb5eb7c6dea689f
Zak <zakeria.ismail1@gmail.com>
2021-08-12 18:44:31 -0400

    fix: next.js and material.ui bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
-import type { AppProps } from 'next/app'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
+import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react'
+import '../styles/globalStyles.css'
 import theme from '../styles/theme'
 
-import '../styles/globalStyles.css'
-
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
-    <div className="body">
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="body">
         <Component {...pageProps} />
-      </ThemeProvider>
-    </div>
+      </div>
+    </ThemeProvider>
   )
 }
 export default MyApp
--- styles/theme.tsx
+++ styles/theme.tsx
@@ -3,7 +3,26 @@ import { createTheme, responsiveFontSizes } from '@material-ui/core'
 import 'typeface-press-start-2p'
 import 'typeface-bangers'
 
+declare module '@material-ui/core/styles/createBreakpoints' {
+  interface BreakpointOverrides {
+    xs: true // removes the `xs` breakpoint
+    sm: true
+    md: true
+    lg: true
+    xl: true
+  }
+}
+
 let theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 321,
+      md: 960,
+      lg: 1280,
+      xl: 1920
+    }
+  },
   typography: {
     h1: { fontFamily: 'Bangers' }
   }
