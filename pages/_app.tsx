bc5f61aab7e997cce21814be84f9ee6066771b52
Zak <zakeria.ismail1@gmail.com>
2021-08-13 12:55:37 -0400

    fix: material ui ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import React, { useEffect } from 'react'
 import '../styles/globalStyles.css'
 import theme from '../styles/theme'
@@ -8,18 +9,27 @@ import theme from '../styles/theme'
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
-      jssStyles.parentElement!.removeChild(jssStyles)
+    if (jssStyles && jssStyles.parentElement) {
+      jssStyles.parentElement.removeChild(jssStyles)
     }
   }, [])
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <div className="body">
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <div className="body">
+          <CssBaseline />
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
   )
 }
 export default MyApp
